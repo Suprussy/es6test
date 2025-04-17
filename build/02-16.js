fc2e3e64a91c96be26adbbd2cb156c92e6824175
Suprussy <suprussy73@gmail.com>
2025-04-17 11:33:57 +0900

    first commit

---
+++ build/02-16.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var name = "홍길동";
+var age = 20;
+var email = "gdhong@test.com";
+
+// let obj = { name: name, age: age, email: email };
+var obj = {
+  name: name,
+  age: age,
+  email: email
+};
+console.log(obj);
